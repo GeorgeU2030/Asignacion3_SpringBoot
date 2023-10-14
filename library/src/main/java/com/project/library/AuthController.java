c74b1ce716d035248a5a4dd28346b7c049b2f077
GeorgeU2030 <georgeu2021@gmail.com>
2023-10-14 16:54:41 -0500

    try-to_auth_jwt

---
+++ library/src/main/java/com/project/library/AuthController.java
@@ -0,0 +1,29 @@
+package com.project.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.security.PermitAll;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PermitAll
+    @PostMapping("/auth")
+    public String authenticate(@RequestBody AuthRequest authRequest) {
+        // Realiza la autenticación (verifica las credenciales) aquí.
+        if (authRequest.getUsername().equals("usuario") && authRequest.getPassword().equals("contrasena")) {
+            // Las credenciales son válidas, genera y devuelve un token JWT.
+            return jwtService.generateToken(authRequest.getUsername());
+        }
+
+        // Si las credenciales no son válidas, puedes devolver un mensaje de error.
+        return "Credenciales no válidas";
+    }
+}
+
---
+++ library/src/main/java/com/project/library/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.project.library;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.Authentication;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request,
+            jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain)
+            throws jakarta.servlet.ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        // Verifica si el token es válido (debe contener la palabra "Bearer" y estar firmado correctamente).
+        if (isValidToken(token)) {
+            Authentication authentication = jwtService.getAuthentication(token);
+
+            if (authentication != null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isValidToken(String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7); // Elimina el prefijo "Bearer "
+            return jwtService.isValidToken(token);
+        }
+
+        return false;
+    }
+
+    
+}
+
