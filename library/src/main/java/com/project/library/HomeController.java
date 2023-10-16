1a80d6bfe524e2f2c25b4468b4398fdd6ceff5ab
GeorgeU2030 <georgeu2021@gmail.com>
2023-10-16 17:38:46 -0500

    config_rols_users

--- library/src/main/java/com/project/library/HomeController.java
+++ library/src/main/java/com/project/library/HomeController.java
@@ -1,19 +1,30 @@
 package com.project.library;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Controller
+@SessionAttributes("token")
 public class HomeController {
 
     @Autowired
@@ -26,41 +37,35 @@ public class HomeController {
 
     @PostMapping("/auth")
     public String authenticate(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session) {
-        // Realiza la autenticación (verifica las credenciales) aquí.
         System.out.println("Username: " + username);
         System.out.println("Password: " + password);
-        if (username.equals("usuario") && password.equals("contrasena")) {
-            // Las credenciales son válidas, genera y devuelve un token JWT.
-            
-            System.out.println("Aqui entre ");
-            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, null);
 
-        // Establece la autenticación en el contexto de seguridad
+        if (username.equals("usuario") && password.equals("contrasena")) {
+            System.out.println("Aqui entre ");
+
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+            org.springframework.security.core.userdetails.User userDetails = new org.springframework.security.core.userdetails.User(username, password, authorities);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             System.out.println(SecurityContextHolder.getContext());
             String token = jwtService.generateToken(username);
-            
-        // Agrega el token como un atributo en la URL de redirección
-        session.setAttribute("token", token);
-        
-        // Redirige a la página de bienvenida
-        return "redirect:/welcome";
+
+            session.setAttribute("token", token);
+            return "redirect:/welcome";
         }
 
-        // Maneja la autenticación fallida
         return "auth";
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/welcome")
-public String welcome(HttpSession session, Model model) {
-    // Recupera el token almacenado en la sesión
-    String token = (String) session.getAttribute("token");
-    
-    // Agrega el token al modelo para que esté disponible en la vista de bienvenida
-    model.addAttribute("token", token);
-
-    return "welcome"; // Devuelve la vista de bienvenida
+    public String welcome(HttpSession session, Model model) {
+        String token = (String) session.getAttribute("token");
+        model.addAttribute("token", token);
+        return "welcome";
+    }
 }
-}
-
--- library/src/main/java/com/project/library/SecurityConfig.java
+++ library/src/main/java/com/project/library/SecurityConfig.java
@@ -3,8 +3,15 @@ package com.project.library;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,30 +22,28 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests// Permitir acceso a /auth sin autenticación
-                    .requestMatchers("/auth").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .formLogin(login -> login
-    .loginPage("/init") // Ruta de la página de inicio de sesión personalizada
-    .permitAll()
-    .successForwardUrl("/welcome") // Redirige a /director después de la autenticación exitosa
-)
-            .logout(logout -> logout
-                .permitAll()
-            );
-
+        .csrf().disable()
+        .authorizeRequests()
+            .requestMatchers("/auth","/init").permitAll()
+            .anyRequest().hasRole("USER")
+            .and()
+        /* .formLogin()
+            .loginPage("/init")
+            .permitAll()
+            .successForwardUrl("/welcome")
+            .and()*/
+        .logout()
+            .permitAll();
+        
         // Agrega tu JwtAuthenticationFilter como un filtro personalizado
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
 
 }
-
 
 }
